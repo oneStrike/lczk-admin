96add9817b84f090e849996143b0d8cb41ad95b9
Akaiito <759994689@qq.com>
2022-05-15 00:33:22 +0800

    chore: build config🐳

--- vite.config.ts
+++ vite.config.ts
@@ -68,7 +68,7 @@ export default defineConfig({
       output: {
         chunkFileNames: 'static/js/[name]-[hash].js',
         entryFileNames: 'static/js/[name]-[hash].js',
-        assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+        // assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
         manualChunks(id) {
           if (id.includes('node_modules')) {
             return id.toString().split('node_modules/')[1].split('/')[0].toString()
